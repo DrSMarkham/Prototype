ed50dba4c07bdc947a747e46a89d4c3d6ebcb526
DrSmarkham <smarkham@live.co.uk>
2016-12-30 14:54:40 +0000

    Add files via upload

---
+++ IteratorResources.java
@@ -0,0 +1,33 @@
+import java.util.*;/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+import static javax.swing.UIManager.get;
+
+/**
+ *
+ * @author DrSMarkham
+ */
+public class IteratorResourses {
+    public static void main(String args[]){
+        SortedMap<String, Integer> aggresources = new SortedMap<String, Integer>();
+        Map<String, Integer> resources = new Map<String, Integer>();
+        //console input roles
+        Set<String>roletypes = new Set<String>();
+        roletypes = resources.keySet();
+        Iterator<String> itr = resources.iterator();
+        while(itr.hasNext()){
+            String keyelement;
+            keyelement = itr.next();
+            Integer sum;
+            Iterator<String,Integer> mapitr = resources.iterator();
+            while(mapitr.containsKey(resources keyelement)){
+            sum+ = get(resources keyelement);
+            Integer remove(resources keyelement);
+                  };
+            aggroles.put(String keyelement, Integer sum)
+        };
+    };
+    
+};
