9b0b40c434969652870d4d4e91d7cdb6f31c14d1
DrSmarkham <smarkham@live.co.uk>
2017-01-15 14:12:17 +0000

    Add files via upload

---
+++ PlanReader.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+import java.io.*;
+
+/**
+ *
+ * @author DrSMarkham
+ */
+class PlanReader {
+    public static void main(String args[]) {
+        
+        try ( FileReader fr = new FileReader("PlanReader.java"))
+        {
+            int c;
+            
+            while((c = fr.read()) != -1) System.out.print((char) c);
+            
+        } catch(IOException e) {
+            System.out.println("I/O Error: " + e);
+        }
+    }
+    
+}
