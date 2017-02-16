72ae5ecf2cbe94e7a90a5bb851c441b1a4a4e544
DrSmarkham <smarkham@live.co.uk>
2017-02-16 13:56:31 +0000

    Add files via upload

---
+++ ReadData.java
@@ -0,0 +1,26 @@
+*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+import java.io.*;
+/**
+ *
+ * @author DrSMarkham
+ */
+public class ReadData {
+    public static void main(String args[]){
+        
+        String s;
+        
+        try(BufferedReader br = new BufferedReader(new FileReader("C:\\Users\\DrSMarkham\\Desktop\\te.txt")))
+        {
+            while((s = br.readLine()) !=null){
+                System.out.println(s);
+            }
+        } catch(IOException exc){
+            System.out.println("I/O Error: " + exc);
+        }
+    }
+    
+}
