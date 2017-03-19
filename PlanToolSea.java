f57bc014dcc17b2997d4e111d8325801c567410b
DrSmarkham <smarkham@live.co.uk>
2017-03-19 14:05:22 +0000

    Add files via upload

--- PlanToolSea.java
+++ PlanToolSea.java
@@ -1,212 +1,240 @@
-
-import java.awt.EventQueue;
+import static com.sun.glass.ui.Cursor.setVisible;
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
 import java.awt.Font;
+import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JTextArea;
-import javax.swing.JTextField;
-import javax.swing.text.AbstractDocument;
+import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import javax.swing.*;
  
-//create class and extend with JFrame
-public class PlanToolSea extends JFrame 
-{
-	//declare variable
-	//private JPanel contentPane;
-	/**
-	 * Launch the application.
-	 */
-	//main method
-	public static void main(String[] args) 
-	{
-		/* It posts an event (Runnable)at the end of Swings event list and is
-		processed after all other GUI events are processed.*/
-		EventQueue.invokeLater(new Runnable()
+public class NewWindowB extends JFrame {
+    
+    JButton b1;
+    JLabel l1;
+    public NewWindowB() {
+        
+        
+
+
+//class PlanToolIB extends JFrame
+//{
+
+    
+
+    //public PlanToolIB() {
+
+        setSize(400,400);
+        setVisible(true);
+
+        setLayout(new BorderLayout());
+
+        JLabel background=new JLabel(new ImageIcon("C:\\Users\\DrSMarkham\\Desktop\\Toulmin.png"));
+
+        add(background);
+
+        background.setLayout(new FlowLayout());
+        
+        
+        JButton btnNewFrame4 = new JButton("Continue");
+		//add actionListener
+		btnNewFrame4.addActionListener(new ActionListener()
 		{
-			public void run()
+			public void actionPerformed(ActionEvent arg0)
 			{
-				//try - catch block
-				//
-                            
-                            try 
-				                             
-                                
-                            {
-					//Create object of NewWindow
-				//	NewWindow1 frame = new NewWindow1();
-					//set frame visible true
-				//	frame.setVisible(false);					
-				}
-				catch (Exception e) 
-				{
-					e.printStackTrace();
-				}
+				//call the object of NewWindow and set visible true
+				//NewWindowB frame = new NewWindowB();
+				//frame.setVisible(true);
+				//set default close operation
+				//setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 			}
 		});
-	}
- 
-	/**
-	 * Create the frame.
-	 */
-	public PlanToolSea() //constructor
-	{
-		//set frame title
-		//setTitle("Semantic Plan Representation");
-		//set default close operation
-		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		//set bounds of the frame
-		//setBounds(200, 200, 450, 300);
-		
-		//create object of JPanel
-		//contentPane = new JPanel();
-		//set border
-		//contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		//set ContentPane
-		//setContentPane(contentPane);
-		//set null
-		//contentPane.setLayout(null);
                 
-                JTextArea textArea = new JTextArea(100, 100);
-                
-                //JTextArea ta = new JTextArea(100, 100);
-    //textArea.setText( "");
-               // "Semantic Plan Representation Tool\n" + "\n" +
+                btnNewFrame4.setFont(new Font("Microsoft YaHei UI", Font.BOLD, 12));
+		//set bounds of the Button
+		btnNewFrame4.setBounds(280, 650, 178, 25);
+
+        l1=new JLabel("Toulmin Diagram");
+        //b1=new JButton("Continue");
+
+        background.add(l1);
+        background.add(btnNewFrame4);
+    }
+
+    public static void main(String args[]) 
+    {
+        new NewWindowB();
+    }
+}
     
-                
-                
-                
-                
-                //JScrollPane scrollPane = new JScrollPane(textArea);
-                JFrame frame = new JFrame("Analogous Search Tool");
-                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-                //frame.setLayout(new BorderLayout());
-                //frame.add(scrollPane);
-                frame.setSize(500, 500);
-                frame.setLocationRelativeTo(null);
-                frame.setVisible(true);
-                
-                
-                
-       
-        //JPanel panel = new JPanel();
 
-        JButton button1 = new JButton("Find Similar Cases(Plans)");
-
-        JTextField answer = new JTextField();
-        answer.setBounds(100, 200, 300, 25);
-        //frame.getContentPane().add(answer); 
-        
-        //frame.add(panel);
-        //panel.add(button1);
-        //panel.add(label);
-        //frame.setVisible(true);
-
-        button1.addActionListener(new ActionListener() {
-
-            public void actionPerformed(ActionEvent arg0) {
-                //JOptionPane.showMessageDialog(frame.getComponent(0), "Hello World");
-                
-                answer.setText("Most Similar Case is: ");
-                //answer.setVisible(true);
-                // ADD RESULT OF SEARCH HERE
+public class PlanToolEntry {
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+             
+            @Override
+            public void run() {
+                BasicForm frame = new BasicForm("Entity Tuples");
+                frame.createGUI();
             }
         });
-     button1.setFont(new Font("Microsoft YaHei UI", Font.BOLD, 12));
-		//set bounds of the Button
-		button1.setBounds(100, 100, 300, 25);
-                
-                JButton clear = new JButton("Clear");
-         clear.addActionListener(new ActionListener(){
-         public void actionPerformed(ActionEvent e){
-         answer.setText("");
+    }
+ 
+}
+ 
+class BasicForm extends JFrame {
+    public BasicForm(String name) {
+        setTitle(name);
+        BasicPanel panel = new BasicPanel();
+        add(panel);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(500,500);
          
     }
-});
-                clear.setFont(new Font("Microsoft YaHei UI", Font.BOLD, 12));
-		//set bounds of the Button
-		clear.setBounds(100, 300, 350, 25);
-        
-     JButton btnNewFrame8 = new JButton("Continue");
+    public void createGUI() {
+        setVisible(true);
+    }
+}
+ 
+class BasicPanel extends JPanel {
+    public BasicPanel() {
+        JButton button = new JButton("Enter New Entity Tuple");
+        button.addActionListener(new ActionListener() {
+             
+            public void actionPerformed(ActionEvent arg0) {
+                InputDialog inputForm = new InputDialog();
+                inputForm.setVisible(true);
+            }
+        });
+        add(button);
+    }
+}
+ 
+class InputDialog extends JDialog {
+    public InputDialog() {
+        InputPanel panel = new InputPanel();
+        setTitle("Input Entity Tuple");
+        add(panel);
+        setSize(500,500);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
+        setVisible(false);
+    }
+}
+ 
+ 
+class InputPanel extends JPanel {
+    JTextField labelField;
+     JTextField roleField;
+     JTextField resourceField;
+     JTextField hazardField;
+     JTextField envfacField;
+      
+    public InputPanel() {
+        GridLayout gridLayout = new GridLayout(6,2);
+        setLayout(gridLayout);
+         labelField = new JTextField(20);
+         roleField = new JTextField(20);
+         resourceField = new JTextField(20);
+         hazardField = new JTextField(20);
+         envfacField = new JTextField(20);
+         
+         JLabel firstNameLabel = new JLabel("Label: ");
+         JLabel secondNameLabel = new JLabel("Roles: ");
+         JLabel phoneNumberLabel = new JLabel("Resources: ");
+         JLabel thirdNameLabel = new JLabel("Hazards: ");
+         JLabel fourthNumberLabel = new JLabel("Environmental Factors: "); 
+         
+         JButton btnNewFrame8 = new JButton("Continue");
 		//add actionListener
 		btnNewFrame8.addActionListener(new ActionListener()
 		{
 			public void actionPerformed(ActionEvent arg0)
 			{
 				//call the object of NewWindow and set visible true
-				NewWindow3 frame = new NewWindow3();
+				NewWindowSem frame = new NewWindowSem();
 				frame.setVisible(true);
 				//set default close operation
-				setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+				//setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 			}
 		});
                 btnNewFrame8.setFont(new Font("Microsoft YaHei UI", Font.BOLD, 12));
 		//set bounds of the Button
 		btnNewFrame8.setBounds(280, 500, 178, 25);
-                
-                JLabel repeat = new JLabel("To find next most similar case press the top button again");
-                repeat.setFont(new Font("Microsoft YaHei UI", Font.BOLD, 12));
-		//set bounds of the Button
-		repeat.setBounds(100, 400, 450, 25);
-                
-                
-                frame.getContentPane().add(textArea);
-                //frame.getContentPane().add(textArea);
-    //add Button into contentPane
-                textArea.add(button1);
-                textArea.add(answer);
-                textArea.add(clear);
-                textArea.add(repeat);
-		textArea.add(btnNewFrame8);
-                
-                frame.pack();
-                frame.setVisible(true);
-                //answer.setVisible(true);
-		((AbstractDocument)textArea.getDocument()).dump(System.out);
-                //Document()).dump(System.out);
-    
-    
-                
-                
-//public class PlanJavaDON {
-  //public static void main(String[] args) {
-    //try {
-    //    UIManager.setLookAndFeel("com.sun.java.swing.plaf.windows.WindowsLookAndFeel");
-    //} catch (Exception evt) {}
-  
-    //JFrame f = new JFrame("Smantic Plan Representation Tool");
-    //JTextArea ta = new JTextArea(100, 100);
-    //ta.setText(
-    //            "Semantic Plan Representation Tool\n" + "\n" +
-    
-//"logic expression.  Actions can only be performed through rules.\n" + "\n" 
-//    );
-    //frame.getContentPane().add(ta);
-    //contentPane.add(ta);
-    //contentPane.pack();
-    //contentPane.setVisible(true);
-
-    //((AbstractDocument)ta.get   
-        
-    }
+         
+         JButton saveToFileButton = new JButton("Save to file");
+          
+         saveToFileButton.addActionListener(new ActionListener() {
+             
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                BufferedWriter writer;
+                try {
+                    //File file = new File("contacts.txt");
+                    //writer = new BufferedWriter(new FileWriter("entitytuples.txt",true));
+                    writer = new BufferedWriter(new FileWriter("C:\\Users\\DrSMarkham\\Desktop\\entitytuples.txt",true));
+                    writer.write(labelField.getText() + "\t" + roleField.getText() + "\t" + 
+                            resourceField.getText() + "\t" + hazardField.getText() + "\t" + envfacField.getText() );
+                    writer.newLine();
+                    writer.close();
+                } catch(FileNotFoundException ex) {
+                    ex.printStackTrace();
+                } catch (IOException ex) {
+                    ex.printStackTrace();
+                }
+                 
+            }
+        });
+         add(firstNameLabel);
+         add(labelField);
+         add(secondNameLabel);
+         add(roleField);
+         add(phoneNumberLabel);
+         add(resourceField);
+         add(thirdNameLabel);
+         add(hazardField);
+         add(fourthNumberLabel);
+         add(envfacField);
+         
+         add(saveToFileButton);
+         add(btnNewFrame8);
+    }                   
+}//JFrame frame = new JFrame("Toulmin Diagram");
+        //frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
  
-
-           
+        //JPanel panel1 = (JPanel)frame.getContentPane();
+ 
+        //JLabel label1 = new JLabel();
+        //label1.setIcon(new ImageIcon("C:\\Users\\DrSMarkham\\Desktop\\Toulmin.png"));// your image here
+        //panel1.add(label1);
+        
                 
-	}
-
-
-
-
-
-
-
-//public TestScrollPane03() {
-        //EventQueue.invokeLater(new Runnable() {
-            //@Override
-            //public void run() {
-                
-          //  }
-        //});
-    //} 
+                //JPanel panel2 = (JPanel)frame.getContentPane();
+                //panel.add(btnNewFrame4);
+                //JLabel label2 = new JLabel();
+                //label2.add(btnNewFrame4);
+                //panel1.add(label2);
+        
+        
+        
+        //frame.setSize(300, 300);
+        
+        //frame.getContentPane().add(textArea);
+    //add Button into contentPane
+        //textArea.add(btnNewFrame4);
+        //frame.getContentPane().add(btnNewFrame4);
+        //frame.setLocationRelativeTo(null);
+        //frame.pack();
+        //frame.setVisible(true);
+        
+        
+    //public static void main (String args[]) {
+    //    SwingUtilities.invokeLater(new Runnable() {
+    //        public void run() {
+    //            new NewWindow4();
+    //        }
+    //    });
+    //}
